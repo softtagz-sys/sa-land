748356e9b2e2ab82b3626805e1abfa47081967b0
Nicolas Verachtert <nicolas.verachtert@student.kdg.be>
2024-09-19 08:57:46 +0200

    Setup Project

---
+++ src/main/java/be/kdg/land/LandApplication.java
@@ -0,0 +1,13 @@
+package be.kdg.land;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LandApplication.class, args);
+    }
+
+}
